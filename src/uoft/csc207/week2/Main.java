dfe5cdcadb4ab2824b2b9d6f1e6a2faad8236e2d
Angela Zhao <113117582+zhaolang05@users.noreply.github.com>
2023-09-12 14:45:12 -0400

    Update Main.java

--- src/uoft/csc207/week2/Main.java
+++ src/uoft/csc207/week2/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String[] name = {"First", "Middle", "Last"};
 	    Person p = new Person(name, "moogah");
+	    Person s = new Student(name, "frooble", "289389892");
         System.out.println(p);
     }
 }
